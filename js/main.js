d0fc54080a2d770fb1032f1c2c65cdf3c77155aa
daiyi <clsun91@gmail.com>
2015-04-01 15:02:18 -0700

    notes tiling with masonry, separated making note and appending note

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready( function() {
       var notes = localStorage['notes'];
       var masonryTile = new Masonry( ".row", {
 	  itemSelector: '.note-wrap'
-        });
+      });
 
       console.log(localStorage);
 
@@ -12,12 +12,13 @@ jQuery(document).ready( function() {
 	  var noteContents = $("#new-note-text").val().replace(/\n/g, '<br/>');
 	  addNote(noteContents, masonryTile);
 	  saveNote(noteContents);
+	  $("#new-note-text").val("");
       });
 
       // populate notes
       if (!notes) {
 	console.log("no notes, creating localStorage");
-	localStorage['notes'] = "['I am a note! (:']";
+	localStorage['notes'] = "['I am a note!']";
       }
       else {
 	console.log("populating notes");
@@ -30,7 +31,16 @@ jQuery(document).ready( function() {
 });
 
 
-function addNote(text, mason) {
+function addNotes(notes, mason) {
+  var i = 0;
+  for (i; i < notes.length; i++) {
+    notes[i].insertAfter($("#new-note-wrap"));
+  }
+  mason.appended(notes);
+  
+}
+
+function makeNote(text) {
   var noteWrap = $("<div>", { class: "col-md-3 col-s-12 note-wrap" });
   var note = $("<div>", { class: "panel panel-success note" });
   var noteHeader = $("<div>", { class: "panel-heading", "align": "right" });
@@ -38,11 +48,9 @@ function addNote(text, mason) {
   var deleteButton = $("<button>", { class: "btn-xs btn-default btn-delete", type: "button" });
   var editButton = $("<button>", { class: "btn-xs btn-default btn-edit", type:"button" });
 
+  var noteObj =  noteWrap.append(note.append(noteHeader.append(editButton.append('edit')).append(deleteButton.append('x'))).append(noteBody.append(text)));
 
-  $("#notes").prepend(noteWrap.append(note.append(noteHeader.append(editButton.append('edit')).append(deleteButton.append('x'))).append(noteBody.append(text))));
-  mason.reloadItems();
-  
-  $("#new-note-text").val("");
+  return noteObj;
 }
 
 
@@ -55,10 +63,11 @@ function saveNote(text) {
 function populateNotes(mason) {
   var noteArray = JSON.parse(localStorage['notes']);
   var i = 0;
+  var notes = []
 
   for (i; i < noteArray.length; i++) {
-    addNote(noteArray[i], mason);
+    notes.push(makeNote(noteArray[i]));
   }
 
-
+  addNotes(notes, mason);
 }
